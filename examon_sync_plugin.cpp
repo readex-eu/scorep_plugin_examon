e505220110705b9cd20dec2bb8f8b0b897b4987c
Lars Jitschin <Lars.Jitschin@tu-dresden.de>
2018-08-07 14:29:28 +0200

    Implemented measuring multiple metrics.

--- examon_sync_plugin.cpp
+++ examon_sync_plugin.cpp
@@ -62,19 +62,30 @@ private:
 	struct mosquitto *mosq = NULL;
 	std::vector<std::string> metricNames;
 	std::vector<std::int32_t> metricIds;
+	std::vector<std::int64_t> metricValues;
+	std::vector<std::int64_t> metricDeltas;
+	std::vector<bool> metricIsEnergy;
+	std::vector<std::int64_t> metricTimestamps;
+	std::vector<std::int64_t> metricElapsed;
 	std::int32_t put_metric(std::string metric)
 	{
-		std::int32_t biggestInt = -1;
-		for(int i = 0; i < metricIds.size(); ++i)
-		{
-			if(metricIds[i] > biggestInt)
-			{
-				biggestInt = metricIds[i];
-			}
-		}
-        std::int32_t newId = biggestInt + 1;
+		std::int32_t biggestInt = 0;
+		if(0 < metricIds.size()) biggestInt = metricIds[metricIds.size() - 1];
+
+		std::int32_t newId = biggestInt + 1;
         metricIds.push_back(newId);
         metricNames.push_back(metric);
+        bool isEnergy = false;
+        if(0 == strncmp("erg", metric.c_str(), 3)
+        && 0 != strncmp("erg_units", metric.c_str(), 9))
+        {
+        	isEnergy = true;
+        }
+        metricIsEnergy.push_back(isEnergy);
+        metricValues.push_back(-1);
+        metricDeltas.push_back(0);
+        metricTimestamps.push_back(0);
+        metricElapsed.push_back(0);
 
         return newId;
 	}
@@ -82,8 +93,7 @@ private:
 	std::string topicMaskPostfix = "/plugin/pmu_pub/chnl";
 	std::string topicBase = "";
 	std::string topicCmd = "";
-	std::string topicErgPkg = "";
-	std::string topicErgUnit = "";
+	std::string topicErgUnits = "";
 	char* hostname = NULL;
 	char* examonHost = NULL;
 	char* connectHost = NULL;
@@ -92,6 +102,10 @@ private:
 	double lastValue = 0;
 	double lastTimestamp = 0;
 	double ergUnit = -1;
+	bool trackErgUnit = false;
+	bool isConnected = false;
+	bool isAlive = true;
+	bool constructorDone = false;
 public:
 	examon_sync_plugin()
 	{
@@ -120,32 +134,64 @@ public:
 
 	    topicBase = configChannel + topicMaskPrefix + hostname + topicMaskPostfix;
 	    topicCmd = topicBase + "/cmd";
-	    topicErgPkg = topicBase + "/data/cpu/0/erg_pkg";
-	    topicErgUnit = topicBase + "/data/cpu/0/erg_units";
+	    topicErgUnits = topicBase + "/data/cpu/0/erg_units";
 
 
 	    mosqpp::lib_init();
 
+	    printf("calling non-async connect\n");
 		connect( connectHost, port, keepalive);
 
+		isAlive = true;
         pthread_t thread_id; /* TODO: store the thread id */
         pthread_create(&thread_id, NULL, &pthread_loop, this);
+        constructorDone = true; //DEBUG
 	}
 	~examon_sync_plugin()
 	{
 		/*  close connection to mosquitto */
+		isAlive = false;
+		isConnected = false;
+		disconnect();
 		mosqpp::lib_cleanup();
 
 	}
+	bool alive() { return isAlive; }
+	bool connected() { return isConnected; }
 	static void* pthread_loop(void* arg)
 	{
 		examon_sync_plugin* myObj = (examon_sync_plugin*) arg;
-		myObj->loop_forever();
+
+		/* Only attempt to subscribe on a successful connect. */
+		while(!myObj->connected())
+		{
+			printf("W");
+			usleep(2000);
+		}
+		if(myObj->alive())
+		{
+			printf("Trying to subscribe metrics.\n");
+		    myObj->subscribe_the_metrics();
+		    myObj->loop_forever();
+		}
+	}
+	void subscribe_the_metrics()
+	{
+        char* subTopic = (char*) malloc(1001);
+		for(int i = 0; i < metricIds.size(); ++i)
+		{
+            sprintf(subTopic, "%s/data/cpu/+/%s", topicBase.c_str(), metricNames[i].c_str());
+            printf("Trying to subscribe to %s.\n", subTopic);
+            subscribe(NULL, subTopic);
+		}
+		free(subTopic);
 	}
 	void on_connect(int rc)
 	{
 		printf("Connected with code %d.\n", rc);
 		if(rc == 0){
+
+			isConnected = true;
 
 			// /usr/bin/mosquitto_pub -t testcluster/node/mabus/plugin/pmu_pub/chnl/cmd -m "-s 0.1"
 		    char* intervalStr = (char *) malloc(100);
@@ -159,15 +205,20 @@ public:
 			/* Disregard desired metrics in metricNames */
 			/* TODO: Do heed the specified metrics */
 
-			/* Only attempt to subscribe on a successful connect. */
-			printf("Trying to subscribe to %s.\n", topicErgPkg.c_str());
-			subscribe(NULL, topicErgPkg.c_str());
-			printf("Trying to subscribe to %s.\n", topicErgUnit.c_str());
-			subscribe(NULL, topicErgUnit.c_str());
+			//printf("Trying to subscribe to %s.\n", topicErgPkg.c_str());
+			//subscribe(NULL, topicErgPkg.c_str());
+			if(trackErgUnit)
+			{
+			    printf("Trying to subscribe to %s.\n", topicErgUnits.c_str());
+    			subscribe(NULL, topicErgUnits.c_str());
+			}
+
 		}
 	}
 	void on_message(const struct mosquitto_message *message)
 	{
+	    char* topicName = basename(message->topic);
+
 		char buf[101];
 
 		//strcmp(message->topic, "channel/name")
@@ -176,34 +227,35 @@ public:
 		memcpy(buf, message->payload, 100*sizeof(char));
 		//printf("Received message, topic: %s, payload: %s\n", message->topic, buf);
 
-		if(0 == strcmp(topicErgPkg.c_str(), message->topic))
-		{
-			if(2 < message->payloadlen)
-			{
-				double readValue;
-				double readTimestamp;
-				double deltaTimestamp = 0;
-				double deltaValue = 0;
-				if(2 == sscanf((char *) message->payload, "%lf;%lf", &readValue, &readTimestamp))
-				{
-					deltaTimestamp = readTimestamp - lastTimestamp;
-					deltaValue = readValue - lastValue;
-
-					lastValue = readValue;
-					lastTimestamp = readTimestamp;
-
-					if(-1 < ergUnit)
-					{
-						printf("Adjusted Energy: %lf Joules\n", deltaValue / deltaTimestamp / ergUnit);
-					}
-
-				}
-		    }
-		} else if(0 == strcmp(topicErgUnit.c_str(), message->topic))
+		if(-1 == ergUnit && 0 == strcmp(topicErgUnits.c_str(), message->topic))
 		{
 			if(1 == sscanf((char *) message->payload, "%lf;%*f", &ergUnit))
 			{
-				unsubscribe(NULL, topicErgUnit.c_str());
+				unsubscribe(NULL, topicErgUnits.c_str());
+			}
+		}
+
+		for(int i = 0; i < metricNames.size(); ++i)
+		{
+			if(0 == strcmp(metricNames[i].c_str(), topicName))
+			{
+				// - read out values from payload
+				double readValue = -1;
+			    double readTimestamp = -1;
+			    int valuesRead = sscanf((char*) message->payload, "%lf;%lf", &readValue, &readTimestamp);
+			    if( 2 == valuesRead )
+			    {
+				    // - calculate delta
+			    	double delta = readValue - metricValues[i];
+			    	metricDeltas[i] = delta;
+			    	delta = readTimestamp - metricTimestamps[i];
+			    	metricElapsed[i] = delta;
+
+     				// - put value in metricValues
+			    	metricValues[i] = readValue;
+			    	metricTimestamps[i] = readTimestamp;
+			    }
+				break;
 			}
 		}
 	}
@@ -220,13 +272,56 @@ public:
     /* return matching properties */
 	std::vector<scorep::plugin::metric_property> get_metric_properties(const std::string& metric_parse)
 	{
+	  //DEBUG
       std::cout << "get_metric_properties(\"" << metric_parse << "\")"<< std::endl;
+      /* valid metrics:
+       * tsc
+       * temp_pkg
+       * erg_dram  -> also track erg_units
+       * erg_cores -> also track erg_units
+       * erg_pkg   -> also track erg_units
+       * erg_units
+       * freq_ref
+       * C2
+       * C3
+       * C6
+       * uclk
+       */
+
       scorep::plugin::util::matcher myMatcher = scorep::plugin::util::matcher(metric_parse);
       std::vector<scorep::plugin::metric_property> foundMatches = std::vector<scorep::plugin::metric_property>();
-      if(myMatcher("tsc") || myMatcher("Joule") || myMatcher("Watt"))
+      std::array<std::string, 11> validMetrics = {"tsc", "temp_pkg", "erg_dram", "erg_cores", "erg_pkg", "erg_units", "freq_ref", "C2", "C3", "C6", "uclk"};
+      std::array<std::string, 11>::iterator iter = validMetrics.begin();
+      for(; iter != validMetrics.end(); ++iter)
       {
-    	  scorep::plugin::metric_property prop = scorep::plugin::metric_property("Joules", "Used energy in Joules", "J");
-    	  foundMatches.push_back(prop);
+    	  if(myMatcher(*iter))
+    	  {
+    		  char* buf = (char*) malloc(101);
+    		  sprintf(buf, "CPU Socket 0 value \"%79s\"", (*iter).c_str());
+    		  buf[100] = '\0';
+    		  char* unit = (char*) "";
+    		  if(0 == strncmp("temp", (*iter).c_str(), 4))
+    		  {
+    			  unit = (char*) "C";
+    		  } else if(0 == strncmp("erg", (*iter).c_str(), 3))
+     		  {
+    			  if(0 != strncmp("erg_units", (*iter).c_str(), 9))
+    			  {
+        			  unit = (char*) "J";
+        			  trackErgUnit = true;
+    			  }
+    		  } else if(0 == strncmp("freq", (*iter).c_str(), 4))
+    		  {
+    			  unit = (char*) "Hz";
+    		  }
+    		  scorep::plugin::metric_property prop = scorep::plugin::metric_property(*iter, buf, unit);
+    		  prop.absolute_point();
+    		  prop.value_double();
+    		  prop.decimal();
+    		  foundMatches.push_back(prop);
+
+    		  free(buf);
+    	  }
       }
 
       return foundMatches;
@@ -236,14 +331,8 @@ public:
 	{
       //DEBUG
 	  std::cout << "add_metric(\"" << metric_name << "\")"<< std::endl;
-      if(metric_name == "Joule"
-      || metric_name == "joule"
-	  || metric_name == "tsc")
-      {
-    	  return put_metric("tsc");
-      }
 
-      return 0; // 0 means failure, I guess
+  	  return put_metric(metric_name);
 	}
 
 
@@ -257,8 +346,23 @@ public:
 	/* get_current_value is the strict variant, value is written to &proxy */
 	template <class Proxy> bool get_optional_value(std::int32_t id, Proxy& p)
 	{
-	  /* TODO: do something */
-      return false;
+	    if(isConnected)
+	    {
+	    	int index = id - 1;
+	    	if(0 <= index && index < metricIds.size() && metricIds[index] == id)
+	    	{
+	    		if(-1 < metricValues[index])
+	    		{
+	    			double calculatedMetric = -1;
+                    if(metricIsEnergy[index]) calculatedMetric = metricDeltas[index] / metricElapsed[index] / ergUnit;
+                    else                      calculatedMetric = metricDeltas[index] / metricElapsed[index];
+                    p.store(calculatedMetric);
+                    printf("Reported Metric %s: %lf\n", metricNames[index].c_str(), calculatedMetric);
+                    return true;
+	    		}
+	    	}
+	    }
+        return false;
 	}
 	/** function to determine the responsible process for x86_energy
 	  *
@@ -276,6 +380,7 @@ public:
 	void synchronize(bool is_responsible, SCOREP_MetricSynchronizationMode sync_mode)
 	{
       /* TODO: Do something */
+		printf("synchronize() called\n");
 	}
 };
 
